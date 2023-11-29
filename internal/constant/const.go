1a488fff4a1a28733760d2582cd250983fe5d245
sorawat <s.tanthikun802@mybvc.ca>
2023-11-29 13:02:15 -0700

    feat: add new constant

--- internal/constant/const.go
+++ internal/constant/const.go
@@ -4,6 +4,16 @@ const (
 	// Response Code
 	SuccessCode = 100
 
+	// Common errors
+	InvalidInputCode  = 200
+	NotFoundCode      = 201
+	UnauthorizedCode  = 202
+	InternalErrorCode = 203
+
 	// Response message
-	SuccessMessage = "success"
+	SuccessMessage  = "success"
+	InvalidInputMsg = "invalid input"
+	NotFoundMsg     = "not found"
+	Unauthorized    = "unauthorized"
+	InternalError   = "internal error"
 )

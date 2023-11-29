9f0e03b233172661cb1d24832820c8442d0e6d0e
sorawat <s.tanthikun802@mybvc.ca>
2023-11-29 13:06:06 -0700

    feat: create menu request model

---
+++ internal/models/menu_request.go
@@ -0,0 +1,25 @@
+package models
+
+import "github.com/shopspring/decimal"
+
+type MenuGetAllResponse struct {
+	CommonResponse
+	Data []MenuGetAllResponseBody `json:"data,omitempty"`
+}
+
+type MenuGetAllResponseBody struct {
+	MenuID int             `json:"menu_id,omitempty"`
+	Name   string          `json:"name,omitempty"`
+	Price  decimal.Decimal `json:"price,omitempty"`
+}
+
+type MenuGetByIdResponse struct {
+	CommonResponse
+	Data MenuGetAllResponseBody `json:"data,omitempty"`
+}
+
+type MenuGetByIdResponseBody struct {
+	MenuID int             `json:"menu_id,omitempty"`
+	Name   string          `json:"name,omitempty"`
+	Price  decimal.Decimal `json:"price,omitempty"`
+}

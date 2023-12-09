f7c0602134b1ab589744f4e8ab2647a9787a86e5
sorawat <s.tanthikun802@mybvc.ca>
2023-12-09 11:19:48 -0700

    refactor: change method name

--- internal/service/service.go
+++ internal/service/service.go
@@ -4,5 +4,5 @@ import "github.com/CLCM3102-Ice-Cream-Shop/backend-product-service/internal/mode
 
 type MenuService interface {
 	GetById(id string) (models.Menu, error)
-	GetAll() ([]models.Menu, error)
+	GetFlavorsAll() ([]models.Menu, error)
 }
